0d0b65f19175d710478c952c95761e73176b1c5d
tomichiruss <lukadayo03@gmail.com>
2024-09-25 02:43:21 -0300

    Texto editado en el conocenos y cambio de bg en proceso.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import PrepagasPage from './pages/PrepagasPage';
 // import CotizadorPage from './pages/CotizadorPage';
-// import ConocenosPage from './pages/ConocenosPage';
+import ConocenosPage from './pages/ConocenosPage';
 import Error404Page from './pages/Error404Page';
 import ScrollToTop from './components/ScrollToTop';
 
@@ -21,8 +21,8 @@ function App() {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/prepagas" element={<PrepagasPage />} />
-                    {/* <Route path="/cotizador" element={<CotizadorPage />} />
-                    <Route path="/conocenos" element={<ConocenosPage />} /> */}
+                    {/* <Route path="/cotizador" element={<CotizadorPage />} /> */}
+                    <Route path="/conocenos" element={<ConocenosPage />} />
                     <Route path="*" element={<Error404Page />} />
                 </Routes>
             </main>
